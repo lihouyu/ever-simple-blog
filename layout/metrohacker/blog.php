2902e46a8f57e01a498aaa053fa05351a2e5b9d4
HouYu Li <karadog@gmail.com>
2009-04-17 11:59:53 +0800

    tinyMCE optimized. full jquery UI lib added.

--- layout/metrohacker/blog.php
+++ layout/metrohacker/blog.php
@@ -1,27 +1,39 @@
 <h1><?php echo $lang['blog_form']; ?></h1>
-<script type="text/javascript" language="javascript" src="tiny-mce/tiny_mce.js"></script>
+<script type="text/javascript" language="javascript" src="tiny_mce/tiny_mce_gzip.js"></script>
+<script type="text/javascript" language="javascript">
+tinyMCE_GZ.init({
+    plugins : "spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,imagemanager,filemanager",
+	themes : "advanced",
+	languages : "en",
+	disk_cache : true,
+	debug : false
+});
+</script>
 <script type="text/javascript" language="javascript">
 <!--
 tinyMCE.init({
-    mode : "textareas",
+    mode : "none",
     theme : "advanced",
     skin : "o2k7",
-    plugins : "safari,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,imagemanager,filemanager",
-    theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontselect,fontsizeselect",
-    theme_advanced_buttons2 : "cut,copy,paste,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-    theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,ltr,rtl,|,fullscreen",
-    theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
+    plugins : "spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,imagemanager,filemanager",
+    theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,sub,sup,|,forecolor,backcolor,|,fontselect,fontsizeselect,|,removeformat,styleprops,|,link,unlink,anchor,image",
+    theme_advanced_buttons2 : "bullist,numlist,|,justifyleft,justifycenter,justifyright,justifyfull,outdent,indent,blockquote,|,ltr,rtl,|,tablecontrols",
+    theme_advanced_buttons3 : "undo,redo,cut,copy,paste,search,replace,|,insertlayer,moveforward,movebackward,absolute,|,insertdate,inserttime,charmap,emotions,iespell,media,advhr,nonbreaking,pagebreak,|,preview,cleanup,code,fullscreen",
     theme_advanced_toolbar_location : "top",
     theme_advanced_toolbar_align : "left",
     theme_advanced_statusbar_location : "bottom",
-    theme_advanced_resizing : true
+    theme_advanced_resizing : true,
+    button_tile_map : true,
+    entity_encoding : "raw",
+    verify_html : false
 });
 
 function toggleEditor(id) {
-    if (!tinyMCE.get(id))
-        tinyMCE.execCommand('mceAddControl', false, id);
-    else
-        tinyMCE.execCommand('mceRemoveControl', false, id);
+    if (!tinyMCE.get(id)) {
+        tinyMCE.execCommand("mceAddControl", false, id);
+    } else {
+        tinyMCE.execCommand("mceRemoveControl", false, id);
+    }
 }
 
 function checkblog(form)
@@ -71,8 +83,9 @@ for ($i = 0; $i < count($categories); $i++)
 ?>
 </select><br />
 <?php echo $lang['content']; ?>:
-<a href="javascript:toggleEditor('content');return false;">[<?php echo $lang['toggle_editor']; ?>]</a><br />
-<textarea id="content" name="content" rows="12" cols="64"><?php echo $blog['content']; ?></textarea><br />
+<a href="javascript:toggleEditor('content');" onclick="toggleEditor('content');return false;">[<?php echo $lang['toggle_editor']; ?>]</a>
+<br />
+<textarea id="content" name="content" rows="18" cols="72"><?php echo $blog['content']; ?></textarea><br />
 <br />
 <input type="submit" name="bsubmit" value="<?php echo $lang['save']; ?>" />
 <input type="hidden" name="b" value="<?php echo $blog['serial']; ?>" />
